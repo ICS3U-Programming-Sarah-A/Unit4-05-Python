ee411d459859a83e26efa85622077f9939d87fe5
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-04-28 21:59:00 +0000

    Made a program that using a loop to add numbers, using continue statements.

--- sum_num_cont.py
+++ sum_num_cont.py
@@ -5,58 +5,61 @@
 # This program asks the user to enter a number to determine how many numbers
 # you want to add. After getting the number, it'll add those numbers until it
 # reaches what user_num entered. Then it calculates all the numbers & displays
-# the sum.
-
+# the sum using loops & continue statements.
 
 def main():
     # initialize the loop counter and sum
+    loop_counter = 0
     sum = 0
-    counter = 0
 
     while True:
-        # get the user number as a string
-        user_number_string_1 = input("How many numbers \
-            would you like to add? ")
+        # get user input as string
+        user_num_1_string = input("How many numbers would you like to add?: ")
         print("")
 
         try:
             # converts user input to integer
-            user_num_int_1 = int(user_number_string_1)
+            user_num_1_int = int(user_num_1_string)
 
-            if user_num_int_1 >= 0:
-                while (counter < user_num_int_1):
-                    # get user input 2
-                    user_num_string_2 = input("Enter a whole number: ")
+            if user_num_1_int >= 0:
+                # calculate the sum of the entered numbers
+                while (loop_counter < user_num_1_int):
+                    # gets input from user
+                    user_num_2_string = input("Enter a whole number: ")
+
                     try:
-                        # converts user input to an integer
-                        user_num_int_2 = int(user_num_string_2)
-                        # sets a range
-                        if user_num_int_2 >= 0:
-                            print("{} has been added to the sum.".
-                                  format(user_num_int_2))
+                        # converts entered number from string to integer
+                        user_num_2_int = int(user_num_2_string)
+
+                        # sets a range, as well as, adds the number entered.
+                        if user_num_2_int >= 0:
+                            print("{} has been added to the sum."
+                                  .format(user_num_2_int))
                             print("")
-                            sum = sum + user_num_int_2
-                            counter = counter + 1
-                            continue
+                            sum = sum + user_num_2_int
+                            loop_counter = loop_counter + 1
                         else:
-                            print("{} is a negative number, so\
-                                it was not added".format(user_num_int_2))
+                            print("{} is negative, so it cannot be added." .
+                                  format(user_num_2_int))
+                            print("")
                             continue
-                    # handles error case
+                    # handles the error case.
                     except Exception:
-                        print("{} is not a valid number".
-                              format(user_num_string_2))
+                        print("{} is not a number.". format(user_num_2_string))
+                        print("")
                         continue
-                    # breaks out of the loop once counter & user reaches max
-                    if counter == user_num_int_1:
-                        print("The sum is = {}.".
-                              format(sum))
-                        break
+                # breaks out of the loop once counter & user reaches max
+                if loop_counter == user_num_1_int:
+                    print("The sum is = {}.".
+                          format(sum))
+                    break
             else:
                 print("Please enter a whole number.")
-        # handles error case
+                print("")
+        # handles the error case.
         except Exception:
-            print("{} is not a valid number.". format(user_number_string_1))
+            print("{} is not a number.". format(user_num_1_string))
+            print("")
 
 
 if __name__ == "__main__":
